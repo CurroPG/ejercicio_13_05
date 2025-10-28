f3d0d89535719d9bd514cfb8c7ff538ba19fbe65
Curro Portillo <211curroportillo@gmail.com>
2025-10-28 11:48:59 +0100

    ejercicio 13

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Por favor, introduce 10 números enteros.");
+        int numeropos = 0;
+        int numeroneg = 0;
+        for (int i = 0; i<10; i++){
+            int numeroIntroducido = Integer.parseInt(System.console().readLine());
+            if (numeroIntroducido <=0)
+                numeroneg++;
+                else
+                    numeropos++;
+        }
+        System.out.println("Ha introducido "+ numeropos +" positivos y "+ numeroneg +" negativos.");
     }
 }
